e9772ffee348e9052506c0db9a0721ecc6e7c9de
guptarnaina <99166825+guptarnaina@users.noreply.github.com>
2023-12-07 22:04:20 +0530

    Update filename2.java

--- filename2.java
+++ filename2.java
@@ -1,1 +1,13 @@
+//adding numbers in a given range using for loop
 
+class Sample
+  {
+    public static void main(String[] args)
+    {
+      int sum=0;
+      for(int i=1;i<=10;i++)
+        {
+          sum=sum+i;
+        }
+    }
+  }
