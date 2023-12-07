810983b973df9f77634246e7be26cc3fa8219533
guptarnaina <99166825+guptarnaina@users.noreply.github.com>
2023-12-07 21:58:12 +0530

    Update filename1.java

--- filename1.java
+++ filename1.java
@@ -1,1 +1,12 @@
+//how to print numbers using for loop
 
+class Sample
+  {
+    public static viod main(String[] args)
+    {
+      for(int i=0;i<10;i++)
+        {
+          System.out.println(i);
+        }
+    }
+  }
