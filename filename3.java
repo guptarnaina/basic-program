b40faf376d62b20caa40d166198e495d31ebcc74
guptarnaina <99166825+guptarnaina@users.noreply.github.com>
2023-12-07 22:09:53 +0530

    Update filename3.java

--- filename3.java
+++ filename3.java
@@ -1,1 +1,18 @@
+//finding the factorial of a given number using while loop
 
+class Sample
+  {
+    public static void main(String[] args)
+    {
+      fact(10);
+    }
+    static void fact(int num)
+    {
+      int mul=1;
+      while(no!=o)
+        {
+          mul=mul*no;
+        }
+      System.out.println(mul);
+    }
+  }
